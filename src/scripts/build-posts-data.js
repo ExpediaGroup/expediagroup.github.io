1f55781a6242c9ca6171da51f152369d88ecf2c6
ffeltrinelli <ffeltrinelli@expediagroup.com>
2022-01-25 17:14:51 +0100

    Sanitize title, creator, link, date

--- src/scripts/build-posts-data.js
+++ src/scripts/build-posts-data.js
@@ -15,18 +15,37 @@ limitations under the License.
 */
 
 const Parser = require('rss-parser');
-const parser = new Parser();
 const moment = require('moment');
 const fs = require("fs");
 
+const MEDIUM_RSS_URL = 'https://medium.com/feed/expedia-group-tech';
+const rssParser = new Parser({
+    customFields: {
+        item: [
+            sanitizedField('title'),
+            sanitizedField('dc:creator', 'creator'),
+            sanitizedField('link')
+        ]
+    }
+});
+
+/**
+ * Configures a field to be parsed so that its HTML is stripped out.
+ * This is a security measure for text-only fields that will be rendered on a web page.
+ * The name of the field in the parsed feed will be added the 'Snippet' suffix
+ * (see https://github.com/rbren/rss-parser#custom-fields for more info).
+ */
+function sanitizedField(sourceFieldName, newFieldName = sourceFieldName) {
+    return [sourceFieldName, newFieldName, {includeSnippet: true}]
+}
 
 async function fetchAndDumpPosts() {
-    const feed = await parser.parseURL('https://medium.com/feed/expedia-group-tech');
+    const feed = await rssParser.parseURL(MEDIUM_RSS_URL);
     const posts = feed.items.map(item => ({
-        title : item.title,
-        creator : item.creator,
-        link : item.link,
-        date : moment(item.isoDate).format('MMM D, YYYY')
+        title : item.titleSnippet,
+        creator : item.creatorSnippet,
+        link : item.linkSnippet,
+        date : moment(item.isoDate, moment.ISO_8601).format('MMM D, YYYY'),
     }))
     fs.writeFile("static/posts.json", JSON.stringify(posts, null, 2), (err) => {
         if (err) {
